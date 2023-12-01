b3a36e0c1ce17d8b462472d94b4a86398492de66
nutbarski <42155399+nutbarski@users.noreply.github.com>
2023-12-01 16:14:33 -0500

    part 3

--- loops.js
+++ loops.js
@@ -1,12 +1,33 @@
-//Part 1 Fizz Buzz
-for (num = 0; num <= 100; num++) {
-    if (num % 3 == 0 && num % 5 == 0) {
-        console.log("Fizz Buzz");
-    } else if (num % 3 == 0) {
-        console.log("Fizz");
-    } else if (num % 5 == 0) {
-        console.log("Buzz");
-    } else {
-        console.log(num);
-    }
-}
+// //Part 1 Fizz Buzz
+// for (num = 0; num <= 100; num++) {
+//     if (num % 3 == 0 && num % 5 == 0) {
+//         console.log("Fizz Buzz");
+//     } else if (num % 3 == 0) {
+//         console.log("Fizz");
+//     } else if (num % 5 == 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(num);
+//     }
+// }
+
+// //part 2 prime time
+
+// for (let n = 2; n < 20; n++) {
+//     let isPrime = true;
+
+//     for (let i = 2; i < n; i++) {
+//         if (n % i === 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+
+//     if (isPrime) {
+//         console.log(n);
+//     }
+// }
+
+//part 3 feeling loopy
+
+let stringData = "ID,Name,Occupation,Age42,Bruce,Knight,4157,Bob,Fry Cook,1963,Blaine,Quiz Master,5898,Bill,Doctor's Assistant,26"
