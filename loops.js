b588bcce15dbe16f2068be30edbe5e9b48e9a037
nutbarski <42155399+nutbarski@users.noreply.github.com>
2023-12-01 23:54:58 -0500

    completed assignment as best as can

--- loops.js
+++ loops.js
@@ -1,33 +1,37 @@
-// //Part 1 Fizz Buzz
-// for (num = 0; num <= 100; num++) {
-//     if (num % 3 == 0 && num % 5 == 0) {
-//         console.log("Fizz Buzz");
-//     } else if (num % 3 == 0) {
-//         console.log("Fizz");
-//     } else if (num % 5 == 0) {
-//         console.log("Buzz");
-//     } else {
-//         console.log(num);
-//     }
-// }
+//Part 1 Fizz Buzz
+for (num = 0; num <= 100; num++) {
+    if (num % 3 == 0 && num % 5 == 0) {
+        console.log("Fizz Buzz");
+    } else if (num % 3 == 0) {
+        console.log("Fizz");
+    } else if (num % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(num);
+    }
+}
 
-// //part 2 prime time
+//part 2 prime time
 
-// for (let n = 2; n < 20; n++) {
-//     let isPrime = true;
+for (let n = 2; n < 20; n++) {
+    let isPrime = true;
 
-//     for (let i = 2; i < n; i++) {
-//         if (n % i === 0) {
-//             isPrime = false;
-//             break;
-//         }
-//     }
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
 
-//     if (isPrime) {
-//         console.log(n);
-//     }
-// }
+    if (isPrime) {
+        console.log(n);
+    }
+}
 
 //part 3 feeling loopy
 
-let stringData = "ID,Name,Occupation,Age42,Bruce,Knight,4157,Bob,Fry Cook,1963,Blaine,Quiz Master,5898,Bill,Doctor's Assistant,26"
+let cells = "ID,Name,Occupation,Age42,Bruce,Knight,4157,Bob,Fry Cook,1963,Blaine,Quiz Master,5898,Bill,Doctor's Assistant,26"
+
+for (let i = 0; i < cells.length; i++){
+    console.log(cells[i]) //i cant figure out how to parse the data into cells!!!1
+}
